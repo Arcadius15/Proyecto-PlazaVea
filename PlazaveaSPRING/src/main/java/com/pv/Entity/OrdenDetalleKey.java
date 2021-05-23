1ac1b2b665cf84ba348ec11266d4e2895b4265dd
Acer <Acer@192.168.1.4>
2021-05-23 10:47:53 -0500

    Implementado hashcode y equals

--- PlazaveaSPRING/src/main/java/com/pv/Entity/OrdenDetalleKey.java
+++ PlazaveaSPRING/src/main/java/com/pv/Entity/OrdenDetalleKey.java
@@ -43,4 +43,36 @@ public class OrdenDetalleKey implements Serializable{
 	public OrdenDetalleKey() {
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((ordenId == null) ? 0 : ordenId.hashCode());
+		result = prime * result + ((productoId == null) ? 0 : productoId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrdenDetalleKey other = (OrdenDetalleKey) obj;
+		if (ordenId == null) {
+			if (other.ordenId != null)
+				return false;
+		} else if (!ordenId.equals(other.ordenId))
+			return false;
+		if (productoId == null) {
+			if (other.productoId != null)
+				return false;
+		} else if (!productoId.equals(other.productoId))
+			return false;
+		return true;
+	}
+	
+
 }
