920c741b37adc6a047367d5f96655c76a40c6549
Diego Vicente <diegovic99@hotmail.com>
2021-06-03 01:28:12 -0500

    Eliminado imports innecesarios.

--- PlazaveaSPRING/src/main/java/com/pv/Controller/UsuarioController.java
+++ PlazaveaSPRING/src/main/java/com/pv/Controller/UsuarioController.java
@@ -1,7 +1,5 @@
 package com.pv.Controller;
 
-import java.net.http.HttpRequest;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -9,13 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.pv.Entity.Cliente;
-import com.pv.Entity.Usuario;
 import com.pv.Service.ClienteService;
 import com.pv.Service.LoginService;
 
